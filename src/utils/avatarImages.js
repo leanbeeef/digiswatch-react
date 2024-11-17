4dd15117f11ae6f4d3a67ef7a1cf8d0e83c51ebf
Ben Jodrey <jodrey48@gmail.com>
2024-11-16 21:41:21 -0400

    updated

--- src/utils/avatarImages.js
+++ src/utils/avatarImages.js
@@ -1,7 +1,7 @@
 // src/utils/avatarImages.js
 const avatars = Array.from({ length: 32 }, (_, i) => ({
-  id: i + 1,
-  src: `${import.meta.env.BASE_URL}src/assets/avatars/avatar (${i + 1}).png`,
-}));
+    id: i + 1,
+    src: `/avatars/avatar (${i + 1}).png`,
+  }));
   
   export default avatars;
